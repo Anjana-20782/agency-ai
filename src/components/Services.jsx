f8077160586cd26dfb179dd51dee9bc601019360
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-30 16:46:31 +0530

    the title file is created

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import assets from "../assets/assets";
+import Title from "./Title.jsx"
 
 const Services=()=>{
 
@@ -38,6 +39,8 @@ const Services=()=>{
         <div id="services" className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 
         xl:px-40 pt-30 text-gray-700 dark:text-white">
             <img src={assets.bgImage2} alt="" className="absolute -top-110 -left-70 -z-1 dark:hidden" />
+
+            <Title title="How can we Help?" desc="From strategy to execution,we carft digital solutions hath movw your bussiness forward"/>
         </div>
     )
 }
