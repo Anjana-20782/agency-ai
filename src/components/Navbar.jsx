d070681a3c2c83e971fcdaa9f4390f2f6e46edd9
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-30 14:29:01 +0530

    new toggle button created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import assets from "../assets/assets";
+import ThemeToggleBtn from "./ThemeToggleBtn";
 
 const Navbar = ({ theme , setTheme}) => {
 
@@ -7,12 +8,12 @@ const Navbar = ({ theme , setTheme}) => {
 
   return (
     <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 
-        xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white">
+        xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-700/70">
 
       <img
         src={theme === "dark" ? assets.logo_dark : assets.logo}
         className="w-32 sm:w-40"
-        alt=""
+        alt="logo"
       />
 
       <div className={`text-gray-500 sm:text-sm ${!sidebarOpen ? 'max-sm:w-0 overflow-hidden' : ' max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0
@@ -28,11 +29,14 @@ const Navbar = ({ theme , setTheme}) => {
         <a onClick={()=>setSidebarOpen(false)} href="#contact-us" className="sm:hover:border-b">Contact Us</a>
       </div>
 
-      <div>
+      <div className="flex items-center gap-2 sm:gap-4">
 
-        <img src={theme=== 'dark ' ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={()=>setSidebarOpen(true)} className="w-8 sm:hidden" />
-        <a href="#contact-us"
-          className="text-sm max-sm:hidden flex items-center gap-2 bg-indigo-500 text-white
+        <ThemeToggleBtn theme={theme} setTheme={setTheme}/>
+
+        <img src={theme === 'dark ' ? assets.menu_icon_dark : assets.menu_icon} alt=""
+         onClick={()=>setSidebarOpen(true)} className="w-8 sm:hidden" />
+
+        <a href="#contact-us" className="text-sm max-sm:hidden flex items-center gap-2 bg-indigo-500 text-white
           px-6 py-2 rounded-full cursor-pointer hover:scale-105 transition-all"
         >
           Connect <img src={assets.arrow_icon} width={14} alt="" />
