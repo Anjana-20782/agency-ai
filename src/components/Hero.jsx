e901e3cc64a780b1b157219cf342a3f193ec9606
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-30 15:04:01 +0530

    a heading created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,17 @@ import assets from "../assets/assets";
 
   const Hero=()=>{
      return(
-        <div id="hero" className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24  xl:p-30 text-center
+        <div id="hero" className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24  xl:p-25 text-center
          w-full overflow-hidden text-gray-400 dark:text-white">
+
             <div className="inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full">
                 <img className="w-20" src={assets.group_profile} alt="" />
                 <p className="text-xs font-bold">Trusted By 10K People</p>
             </div>
+
+            <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl">Turning imagination into 
+                <span className=""> digital </span>
+                impact.</h1>
         </div>
      )
   }
