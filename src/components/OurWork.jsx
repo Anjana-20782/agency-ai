30ac6d350c337659d5e08bd54bdcaac2727733d6
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-31 11:20:29 +0530

    our work new contents added

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import Title from "./Title";
+import assets from "../assets/assets";
 
 const OurWork=()=>{
+
+    const workData = [
+        {
+            title:'Mobile app marketing',
+            description:"We specialize in mobile app marketing to help fitness apps reach more users and drive engagement",
+            image:assets.work_mobile_app
+
+        },
+         {
+            title:'Dashboard managment',
+            description:"we provide dashboard management to monitor performance in real time",
+            image:assets.work_dashboard_management
+
+        },
+         {
+            title:'Fitness app promotion',
+            description:"Our focus on fitness app promotion targets health-conscious , engaging content.",
+            image:assets.work_fitness_app
+
+        }
+        
+
+    ]
     return(
         <div id='our-work' className="flex flex-col items-center gap-7 px-4 sm:px-12 lg-px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
             <Title title='Our latest work' desc='Fro startegy to execution,we creaft digital solutions that move your business forward.' />
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
+                {
+                    workData.map((work,index)=>(
+                        <div key={index} className="hover:scale-102 duration-500 
+                        transition-all cursor-pointer">
+                            
+                            <img src={work.image} className="w-full rounded-xl" alt="" />
+                            <h3 className="mt-3 mb-2 text-lg font-semibold">{work.title}</h3>
+                            <p className="text-sm text-gray=400 opacity-60 w-5/6">{work.description}</p>
+                        </div>
+                    ))
+                }
+
+            </div>
 
         </div>
     )
